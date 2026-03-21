0ef95596b8df2aac6ccce48e58d0a8062221b8d6
tsilizydan <150443526+tsilizydan@users.noreply.github.com>
2026-03-21 15:51:02 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  */
 return [
     'name'     => env('APP_NAME', 'Vanilla Group Madagascar'),
-    'url'      => env('APP_URL', 'https://vanillagroup-madagascar.tsilizy.com'),
+    'url'      => env('APP_URL', 'http://vanillagroup-madagascar.tsilizy.com'),
     'env'      => env('APP_ENV', 'production'),
     'debug'    => filter_var(env('APP_DEBUG', false), FILTER_VALIDATE_BOOLEAN),
     'timezone' => env('APP_TIMEZONE', 'Indian/Antananarivo'),
